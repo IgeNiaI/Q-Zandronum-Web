26e87212a1f910fd4938546f72975d237c31e16f
Gin Fuyou <webmaster@doratoa.net>
2020-12-19 00:43:42 +0300

    bump version

--- qzandronum/settings.py
+++ qzandronum/settings.py
@@ -21,7 +21,7 @@ from django import get_version as django_version
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import gettext_lazy as _
 
-__version__ = "0.12.2-b-2"
+__version__ = "0.20.1-b"
 
 cbs.DEFAULT_ENV_PREFIX = 'QZANDRONUM_'
 
