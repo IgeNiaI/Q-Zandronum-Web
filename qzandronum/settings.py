d37faff39ed6e5b4ab4bb496cce03a4cf660fd87
Gin Fuyou <webmaster@doratoa.net>
2023-07-13 03:38:53 +0300

    update alphatius link

--- qzandronum/settings.py
+++ qzandronum/settings.py
@@ -21,7 +21,7 @@ from django import get_version as django_version
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import gettext_lazy as _
 
-__version__ = "0.54.2a0"
+__version__ = "0.54.2a1"
 
 cbs.DEFAULT_ENV_PREFIX = 'QZANDRONUM_'
 
